f3940d3ce7353294c3d4807d361020995eb3e6ca
Leslye <leslimdf@hotmail.com>
2024-07-17 10:44:10 -0300

    refactor: removed non utilized imports

--- ui-assessment-master/src/components/header.jsx
+++ ui-assessment-master/src/components/header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const HeaderContainer = styled.div`
   display: flex;
